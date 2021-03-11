11eebbafa9d78b72edd8304ad223f0ffad1a14dc
Rinaldo Sassaroli <rinaldo@icheckgateway.com>
2021-03-11 16:39:58 -0300

    Add project files.

---
+++ Controllers/ForecastController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Upstart13.Weather.API.Models;
+
+namespace Upstart13.Weather.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ForecastController : ControllerBase
+    {
+
+        private readonly ILogger<ForecastController> _logger;
+
+        public ForecastController(ILogger<ForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("/Forecast/{address}")]
+        public async Task<IActionResult> GetAsync(string address)
+        {
+
+            string urlGetCoordinates = $"https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address={address}&benchmark=Public_AR_Current&format=json";
+            
+            var _httpClient = new HttpClient();
+
+            var response = await _httpClient.GetAsync(urlGetCoordinates);
+            var content = await response.Content.ReadAsStringAsync();
+
+            GeocodingResult GCResult = JsonConvert.DeserializeObject<GeocodingResult>(content);
+
+            if (GCResult != null && GCResult.result.addressMatches.Count > 0)
+            {
+
+                string x = GCResult.result.addressMatches[0].coordinates.x.ToString();
+                string y = GCResult.result.addressMatches[0].coordinates.y.ToString();
+
+                string urlGetGrid = $"https://api.weather.gov/points/{y},{x}";
+
+                _httpClient.DefaultRequestHeaders.Add("User-Agent", "(upstart13.com, info@upstart13.com)");
+                _httpClient.DefaultRequestHeaders.Add("Accept", "application/geo+json");
+
+                response = await _httpClient.GetAsync(urlGetGrid);
+                content = await response.Content.ReadAsStringAsync();
+
+                GridResult GResult = JsonConvert.DeserializeObject<GridResult>(content);
+                if (GResult != null)
+                {
+                    string gridId = GResult.properties.gridId.ToString();
+                    string gridX = GResult.properties.gridX.ToString();
+                    string gridY = GResult.properties.gridY.ToString();
+
+                    string urlGetWeather = $"https://api.weather.gov/gridpoints/{gridId}/{gridX},{gridY}/forecast";
+
+                    response = await _httpClient.GetAsync(urlGetWeather);
+                    content = await response.Content.ReadAsStringAsync();
+
+                    ForecastResult FResult = JsonConvert.DeserializeObject<ForecastResult>(content);
+                    if (FResult != null && FResult.properties.periods.Count > 0)
+                    {
+                        return Ok(content);
+                    } else {
+                        return NotFound("Forecast not found");
+                    }
+                } else {
+                    return NotFound("Grid not found");
+                }
+            } else {
+                return NotFound("Address not found");
+            }
+        }
+    }
+}
---
+++ Models/GridResult.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Upstart13.Weather.API.Models
+{
+    public class GridResult
+    {
+        [JsonProperty("@context")]
+        public List<object> Context { get; set; }
+        public string id { get; set; }
+        public string type { get; set; }
+        public Geometry1 geometry { get; set; }
+        public Properties1 properties { get; set; }
+    }
+
+    public class Geometry1
+    {
+        public string type { get; set; }
+        public List<double> coordinates { get; set; }
+    }
+
+    public class Distance
+    {
+        public double value { get; set; }
+        public string unitCode { get; set; }
+    }
+
+    public class Bearing
+    {
+        public int value { get; set; }
+        public string unitCode { get; set; }
+    }
+
+    public class Properties2
+    {
+        public string city { get; set; }
+        public string state { get; set; }
+        public Distance distance { get; set; }
+        public Bearing bearing { get; set; }
+    }
+
+    public class Properties1
+    {
+        [JsonProperty("@id")]
+        public string Id { get; set; }
+
+        [JsonProperty("@type")]
+        public string Type { get; set; }
+        public string cwa { get; set; }
+        public string forecastOffice { get; set; }
+        public string gridId { get; set; }
+        public int gridX { get; set; }
+        public int gridY { get; set; }
+        public string forecast { get; set; }
+        public string forecastHourly { get; set; }
+        public string forecastGridData { get; set; }
+        public string observationStations { get; set; }
+        public RelativeLocation relativeLocation { get; set; }
+        public string forecastZone { get; set; }
+        public string county { get; set; }
+        public string fireWeatherZone { get; set; }
+        public string timeZone { get; set; }
+        public string radarStation { get; set; }
+    }
+
+    public class RelativeLocation
+    {
+        public string type { get; set; }
+        public Geometry1 geometry { get; set; }
+        public Properties2 properties { get; set; }
+    }
+
+
+
+}
